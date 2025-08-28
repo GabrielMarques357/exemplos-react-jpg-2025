6b9d76dd3a69a88cb8ca6687574214813d37cb74
Gabriel <gabriel.elane3106@gmail.com>
2025-08-27 21:56:24 -0300

    add/router-dom

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,18 +1,22 @@
 import './style.css'
-import Header from '../../componets/Header'
-import Footer from '../../componets/Footer'
+
 import List from '../../componets/List'
 import Images from '../../componets/Images'
 import Form from '../../componets/Form'
+import { Link } from 'react-router-dom'
 
 function Home() {
     return (
         <>
 
-            <Header />
+            
 
 
             <main>
+                <Link to='/About'>
+                <button type="button">Navegar</button>
+                </Link>
+
                  <List titulo="Batata" />
                 <List titulo="Aipim" />
                 <Images />
@@ -20,7 +24,7 @@ function Home() {
             </main>
 
 
-            <Footer />
+            
 
         </>
     )
